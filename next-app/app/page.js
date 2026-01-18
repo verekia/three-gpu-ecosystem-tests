98454ee78c868eae1985c614edbdb1c66f96e973
verekia <522007+verekia@users.noreply.github.com>
2026-01-18 16:21:48 +0700

    feat: add wobbly sphere

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -1,17 +1,36 @@
 'use client'
 
-import { Canvas } from '@react-three/fiber/webgpu'
+import { Canvas, useUniforms, useNodes, useLocalNodes } from '@react-three/fiber/webgpu'
 import { OrbitControls } from '@react-three/drei/core'
+import { Fn, vec3, sin, time, positionLocal, normalLocal } from 'three/tsl'
 
+const GlobalEffects = () => {
+  useNodes(() => ({
+    wobble: Fn(() => vec3(sin(time), sin(time.mul(1.3)), sin(time.mul(2)))),
+  }))
+
+  return null
+}
+
+const WobblySphere = () => {
+  const { uIntensity } = useUniforms({ uIntensity: 0.5 })
+
+  const { displacement } = useLocalNodes(({ nodes }) => ({
+    displacement: nodes.wobble().mul(uIntensity),
+  }))
+
+  return (
+    <mesh>
+      <sphereGeometry />
+      <meshBasicNodeMaterial positionNode={positionLocal.add(normalLocal.mul(displacement))} color="red" />
+    </mesh>
+  )
+}
 const IndexPage = () => (
   <Canvas style={{ height: '100vh' }}>
     <OrbitControls />
-    <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color="red" />
-    </mesh>
+    <GlobalEffects />
+    <WobblySphere />
   </Canvas>
 )
-
 export default IndexPage
-
